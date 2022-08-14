cbcb634dfbba8fecd18df4e188239179c4b857cd
PuteraSyabil <puteramuhammadsyabil@gmail.com>
2022-08-14 22:51:37 +0800

    color

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
@@ -7,6 +10,36 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+
+
+import {CardModule} from 'primeng/card';
+import {ToolbarModule} from 'primeng/toolbar';
+import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
+import { CategoriesService } from '@bluebits/products';
+import { CategoriesFormComponent } from './categories/categories-form/categories-form.component';
+import {InputTextModule} from 'primeng/inputtext';
+import {ToastModule} from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import {ConfirmationService} from 'primeng/api';
+import {ColorPickerModule} from 'primeng/colorpicker';
+
+
+const UX_MODULE=[
+    CardModule,
+    ToolbarModule,
+    ButtonModule,
+    TableModule,
+    InputTextModule,
+    ToastModule,
+    ConfirmDialogModule,
+    ColorPickerModule
+
+]
 
 const routes: Routes=[
     {
@@ -16,16 +49,40 @@ const routes: Routes=[
             {
                 path:'dashboard',
                 component: DashboardComponent
+            },
+            {
+                path:'categories',
+                component:CategoriesListComponent,
+               
+            },
+            {
+                path:'categories/form',
+                component:CategoriesFormComponent
+               
+            },
+            {
+                path:'categories/form/:id',
+                component:CategoriesFormComponent
+               
             }
         ]
-    }
+    },
+    
     
 ];
 
 @NgModule({
-    declarations: [AppComponent, NxWelcomeComponent, DashboardComponent, ShellComponent, SidebarComponent],
-    imports: [BrowserModule, RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' })],
-    providers: [],
+    declarations: [AppComponent, NxWelcomeComponent, DashboardComponent, ShellComponent, SidebarComponent, CategoriesListComponent, CategoriesFormComponent],
+    imports: [
+        BrowserModule, 
+        BrowserAnimationsModule,
+        RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
+        ...UX_MODULE,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    providers: [CategoriesService,MessageService,ConfirmationService],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
